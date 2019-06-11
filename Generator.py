ed37f072a232e4fa69210da468ea2954cf117751
Youri Arkesteijn <youri.essa@gmail.com>
2019-06-11 13:00:57 +0200

    Update generator to create batches

--- Generator.py
+++ Generator.py
@@ -1,18 +1,36 @@
 import random
 import sys
+import os
 
-assert len(sys.argv) > 2, "Please supply: file name, number of instances, followed by number of objectives"
+# show error on incomplete arguments
+assert len(sys.argv) > 2, "Please supply: folder name, maximum number of instances, " \
+                          "followed by maximum number of objectives"
+# load arguments
+instanceName = sys.argv[1]
+maxn = int(sys.argv[2])
+maxm = int(sys.argv[3])
 
-f = sys.argv[1]
-n = int(sys.argv[2])
-m = int(sys.argv[3])
+# Create target Directory if don't exist
+if not os.path.exists(instanceName):
+    os.mkdir(instanceName)
 
-f = open(f,"w+")
-f.write(str(n) + " " + str(m) + "\n")
+# for all combinations of n and objectives
+for n in range(1, maxn):
+    for m in range(1, maxm):
 
-for o in range(m):
-    for i in range(n):
-        for j in range(i):
-            f.write(str(random.randint(1, sys.maxsize)) + "\n")
+        # make an instance
+        f = open(instanceName + "/instance_" + str(n) + "_" + str(m) + ".tsp", "w+")
+        f.write(str(n) + " " + str(m) + "\n")
 
-f.close()
+        # only upper trinagle matrix needs to be represented
+        for o in range(m):
+            for i in range(n):
+                for j in range(i):
+                    f.write(str(random.randint(1, n*100)) + "\n")
+
+        # close file
+        f.close()
+
+    # log our progress
+    print("Made all instances up to size " + str(n))
+print("Done")
